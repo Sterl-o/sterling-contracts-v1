6720e216012d23262b4d640250593063bcb15fbf
MyNameIsKellKell <86309030+chimpydev@users.noreply.github.com>
2023-02-19 22:34:17 +0000

    Update StrPair.sol

--- contracts/base/core/StrPair.sol
+++ contracts/base/core/StrPair.sol
@@ -123,7 +123,7 @@ contract StrPair is IERC20, IPair, Reentrancy {
   }
 
   function setSwapFeeChosen(uint _swapFeeChosen) external {
-    require(IFactory(factory).treasury() == msg.sender, "StrPair: PAUSE");
+    require(IFactory(factory).treasury() == msg.sender, "StrPair: not StrFactory treasury");
     require(_swapFeeChosen > 50, "StrPair: Fee too high amount / _swapFeeChosen = feePaidAmount")
     swapFeeChosen = _swapFeeChosen;
   }
