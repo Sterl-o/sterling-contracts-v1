08a510779f83ca363a6ec6693c2b61b2031114eb
MyNameIsKellKell <86309030+chimpydev@users.noreply.github.com>
2023-02-23 12:08:08 +0000

    fiiiix

---
+++ scripts/verifyArgs.ts
@@ -0,0 +1,4 @@
+/* coconut contract testnet deploy arguments
+ * npx hardhat verify --constructor-args scripts/args.ts  0x9b19319E9bcFf85D106956096286A554a3C43350 --network fantom
+ **/
+module.exports = ["0x24328D39AbB27E3EAf50Ebd778d9A598276CDd59", "0x3C69f76B462201572e8d40c2C90e78Fd95395AF6","0xb5c6C0D63F3c10659E5E6fDcE08477bE33E334F5"];
